d89f23ad757e96ffe821d09964e4415dd9a0bb51
yinm <yinm.cminor9@gmail.com>
2017-08-25 06:27:08 +0900

    add post text

---
+++ chapter3/06.post-any-body-text/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	reader := strings.NewReader("テキスト")
+
+	resp, err := http.Post("http://localhost:18888", "text/plain", reader)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Status:", resp.Status)
+}
