7abb4f6bf6eb1eb177db64eebb2549b592aac29a
yinm <yinm.cminor9@gmail.com>
2017-08-28 08:10:13 +0900

    add request dump

--- chapter2/cookie.go
+++ chapter2/cookie.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Set-Cookie", "VISIT=TRUE")
+
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+
 	if _, ok := r.Header["Cookie"]; ok {
 		// Cookieがあるということは一度来たことがある人
 		fmt.Fprintf(w, "<html><body>2回目以降</body></html>\n")
