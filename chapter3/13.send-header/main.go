2a5efe3651f075114f1d2c1dc754f3456a1e39e5
yinm <yinm.cminor9@gmail.com>
2017-09-03 21:34:44 +0900

    Add sending header
    自分で書いたので、あってるかは微妙...！

---
+++ chapter3/13.send-header/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", "http://localhost:18888", nil)
+	if err != nil {
+		panic(err)
+	}
+	request.Header.Add("Content-Type", "image/jpeg")
+
+	resp, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(string(dump))
+}
