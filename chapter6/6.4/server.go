07e1ba4db1c7faeeaa02050b0abcbe5385618395
yinm <yinm.cminor9@gmail.com>
2017-10-28 15:33:05 +0900

    Add 6.4

---
+++ chapter6/6.4/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println(string(dump))
+	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
+}
+
+func handlerChunkedResponse(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		panic("expected http.ResponseWriter to be an http.Flusher")
+	}
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(w, "Chunk #%d\n", i)
+		flusher.Flush()
+		time.Sleep(500 * time.Millisecond)
+	}
+	flusher.Flush()
+}
+
+func main() {
+	var httpServer http.Server
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/chunked", handlerChunkedResponse)
+	log.Println("start http listening :18888")
+	httpServer.Addr = ":18888"
+	log.Println(httpServer.ListenAndServe())
+}
