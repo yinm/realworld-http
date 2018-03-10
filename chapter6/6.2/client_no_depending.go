fa8327bca29eb4dc2ff32d6e826a7f8191ca1b7b
yinm <yinm.cminor9@gmail.com>
2018-03-10 21:50:04 +0900

    Add client not depending on OS

---
+++ chapter6/6.2/client_no_depending.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	// read certificate
+	cert, err := ioutil.ReadFile("ca.crt")
+	if err != nil {
+		panic(err)
+	}
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(cert)
+	tlsConfig := &tls.Config{
+		RootCAs: certPool,
+	}
+	tlsConfig.BuildNameToCertificate()
+
+	// create Client
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+
+	// do communicate
+	resp, err := client.Get("https://localhost:18443")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(dump))
+}
