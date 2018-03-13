6573e4846f8bb22ca00fa2dccb19b7b177662dfc
yinm <yinm.cminor9@gmail.com>
2018-03-14 08:58:03 +0900

    Check 2nd

--- chapter6/6.3/client.go
+++ chapter6/6.3/client.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	// TCPソケットをオープン
 	dialer := &net.Dialer{
-		Timeout: 30 * time.Second,
+		Timeout:   30 * time.Second,
 		KeepAlive: 30 * time.Second,
 	}
 	conn, err := dialer.Dial("tcp", "localhost:18888")
