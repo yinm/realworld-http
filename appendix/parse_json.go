33883d4759c6c5c7193b05b894828d5ed54b2a39
yinm <yinm.cminor9@gmail.com>
2018-03-18 19:56:47 +0900

    Add parsing JSON

---
+++ appendix/parse_json.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Book struct {
+	Title  string `json: "title"`
+	Author string `json: "author"`
+}
+
+var jsonString = []byte(`
+[
+	{"title": "The Art of Community", "author": "Jono Bacon"},
+	{"title": "Mithril", "author": "Yoshiki Shibukawa"}
+]`)
+
+func main() {
+	var books []Book
+	err := json.Unmarshal(jsonString, &books)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, book := range books {
+		fmt.Println(book)
+	}
+}
